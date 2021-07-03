5179da3f86e3c6fae88e0aa1d2fa9262f88148d0
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2021-07-04 00:09:56 +0600

    design improved

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,24 +10,31 @@ interface SetMethod {
   (v: PDFDocument): any;
 }
 
-const getPdfUrl = async (doc: PDFDocument) => {
-  const bytes = await doc.save();
-  return URL.createObjectURL(new Blob([bytes], { type: "application/pdf" }));
-};
-
-const setPdfUrl = async (doc: PDFDocument, settr: any) => {
-  const url = await getPdfUrl(doc);
-  if (settr) settr(url);
-};
-
 export default function Home() {
-  const [topPDF, setTopPDF] = useState<PDFDocument>();
+  const [topPDF, setTopPDF] = useState<PDFDocument | null>();
   const [topPdfUrl, setTopPdfUrl] = useState<string>("");
-  const [scriptPDF, setScriptPDF] = useState<PDFDocument>();
+  const [scriptPDF, setScriptPDF] = useState<PDFDocument | null>();
   const [scriptPdfUrl, setScriptPdfUrl] = useState<string>("");
-  const [mergedPDF, setMergedPDF] = useState<PDFDocument>();
+  const [mergedPDF, setMergedPDF] = useState<PDFDocument | null>();
   const [mergedPdfUrl, setMergedPdfUrl] = useState<string>("");
   const [error, setError] = useState<string>("");
+
+  const getPdfUrl = async (doc: PDFDocument) => {
+    try {
+      const bytes = await doc.save();
+      return URL.createObjectURL(
+        new Blob([bytes], { type: "application/pdf" })
+      );
+    } catch (e) {
+      console.log(e);
+      setError(e.toString());
+    }
+  };
+
+  const setPdfUrl = async (doc: PDFDocument, settr: any) => {
+    const url = await getPdfUrl(doc);
+    if (settr) settr(url);
+  };
 
   useEffect(() => {
     if (topPDF) {
@@ -104,8 +111,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="h-screen">
-        <div className="py-4 flex justify-center gap-3">
+      <main className="h-screen flex flex-col text-2xs">
+        {/* <div className="py-4 flex justify-center gap-3">
           <Image
             src="https://upload.wikimedia.org/wikipedia/commons/5/51/LogoAust.jpg"
             width="38"
@@ -113,89 +120,120 @@ export default function Home() {
             alt="AUST Logo"
           />
           <h1 className="text-center text-4xl font-bold ">PDF merger</h1>
-        </div>
-        <div className="grid lg:grid-cols-2 h-3/6">
+        </div> */}
+        <div className="lg:grid lg:grid-cols-2 flex-grow sm:h-3/6 ">
           <div className="flex flex-col justify-center items-center border-r-4 border-gray-600">
             <label
               htmlFor="topPage"
-              className="text-xl mb-auto bg-gray-700 w-full text-center text-white py-5 select-none cursor-pointer"
+              className=" lg:text-xl bg-gray-700 w-full text-center text-white py-5 select-none cursor-pointer"
             >
               Select Top Page
             </label>
-            {!topPDF && (
-              <input
-                id="topPage"
-                type="file"
-                onChange={(e) => fileChangeHandler(e)}
-                accept=".pdf"
-                className="mb-auto ml-24"
-              />
-            )}
-            {topPDF && topPdfUrl && (
-              <div className="mb-auto h-2/3">
-                <PdfViewer url={topPdfUrl} />
-                <h3 className="text-center">{topPDF.getPageCount()} Pages</h3>
-              </div>
-            )}
+            <div className="flex-grow bg-white flex items-center">
+              {!topPDF && (
+                <div className="flex justify-center w-full items-center h-24 lg:h-auto">
+                  <input
+                    id="topPage"
+                    type="file"
+                    onChange={(e) => fileChangeHandler(e)}
+                    accept=".pdf"
+                    className="w-10/12"
+                  />
+                </div>
+              )}
+              {topPDF && topPdfUrl && (
+                <div className="sm:h-5/6">
+                  <PdfViewer url={topPdfUrl} />
+                  <h3 className="text-center bg-black text-white relative">
+                    {topPDF.getPageCount()} Pages
+                    <button
+                      className="ml-auto absolute right-1 text-red-400"
+                      onClick={() => setTopPDF(null)}
+                    >
+                      Remove
+                    </button>
+                  </h3>
+                </div>
+              )}
+            </div>
           </div>
           <div className="flex flex-col justify-center items-center">
             <label
               htmlFor="scriptPage"
-              className="text-xl mb-auto bg-gray-500 w-full text-center text-white py-5 select-none cursor-pointer"
+              className=" lg:text-xl bg-gray-700 w-full text-center text-white py-5 select-none cursor-pointer"
             >
               Select Script Page
             </label>
-            {!scriptPDF && (
-              <input
-                id="scriptPage"
-                type="file"
-                onChange={(e) => fileChangeHandler(e)}
-                accept=".pdf"
-                className="mb-auto ml-24"
-              />
-            )}
-            {scriptPDF && scriptPdfUrl && (
-              <div className="mb-auto h-2/3">
-                <PdfViewer url={scriptPdfUrl} />
-                <h3 className="text-center">
-                  {scriptPDF.getPageCount()} Pages
-                </h3>
-              </div>
-            )}
+            <div className="flex-grow bg-white flex items-center">
+              {!scriptPDF && (
+                <div className="flex justify-center w-full items-center h-24 lg:h-auto">
+                  <input
+                    id="scriptPage"
+                    type="file"
+                    onChange={(e) => fileChangeHandler(e)}
+                    accept=".pdf"
+                    className="w-10/12"
+                  />
+                </div>
+              )}
+              {scriptPDF && scriptPdfUrl && (
+                <div className="sm:h-5/6">
+                  <PdfViewer url={scriptPdfUrl} />
+                  <h3 className="text-center bg-black text-white relative">
+                    {scriptPDF.getPageCount()} Pages
+                    <button
+                      className="ml-auto absolute right-1 text-red-400"
+                      onClick={() => setScriptPDF(null)}
+                    >
+                      Remove
+                    </button>
+                  </h3>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-        <div className="grid grid-cols-1">
-          {error && (
-            <p className="text-red-500 text-center text-2xl bg-black py-1">
-              {error}
-            </p>
-          )}
-        </div>
-        <div className="bg-gray-600 h-3/6 flex gap-24 items-center justify-center">
+        {error && (
+          <div className="absolute p-5 bg-black left-0 bottom-0 lg:left-7 lg:bottom-7">
+            <p className="text-red-500 text-center text-sm py-1">{error}</p>
+          </div>
+        )}
+        <div className="bg-gray-600 h-5/6 sm:h-3/6 flex sm:flex-row gap-24 items-center justify-center">
           {!mergedPdfUrl && (
             <button
-              className="bg-green-600 p-5 px-32 text-white text-3xl rounded-lg"
+              className="bg-green-600 p-5 px-32 text-white lg:text-3xl rounded-lg"
               onClick={merge}
             >
               Merge
             </button>
           )}
           {mergedPdfUrl && (
-            <>
-              <div className="h-2/3">
+            <div className="flex flex-col lg:flex-row sm:gap-24 sm:h-2/3 items-center">
+              <div className="self-stretch">
                 <PdfViewer url={mergedPdfUrl} />
-                <h3 className="text-center">
+                <h3 className="text-center text-white bg-black">
                   {mergedPDF?.getPageCount()} Pages
                 </h3>
               </div>
-              <a
-                href={mergedPdfUrl}
-                className="bg-green-600 p-5 px-32 text-white text-3xl rounded-lg"
-                download
-              >
-                Download
-              </a>
-            </>
+              <div className="flex flex-col">
+                <a
+                  href={mergedPdfUrl}
+                  className="bg-green-600 text-md p-5 self-stretch lg:self-auto text-center px-28 text-white lg:text-3xl rounded-lg"
+                  download
+                >
+                  Download
+                </a>
+                <button
+                  className="self-end hover:text-white"
+                  onClick={() => {
+                    setMergedPDF(null);
+                    setMergedPdfUrl("");
+                  }}
+                >
+                  Merge Again?
+                </button>
+              </div>
+            </div>
           )}
         </div>
       </main>
